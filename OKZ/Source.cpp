4d4494f6b5f9cc1c8211bc722daba4fef3101b17
let little blonde code something <nika-by-niksa@mail.ru>
2018-12-01 13:37:58 +0300

    Sent

--- OKZ/Source.cpp
+++ OKZ/Source.cpp
@@ -4,16 +4,15 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <fstream>
-#include <ctime>
 #include <chrono>
 using namespace cv;
 using namespace std;
-
+using namespace std::chrono;
 
 
 int main()
 {
-	auto begin = chrono::high_resolution_clock::now();
+	auto start = high_resolution_clock::now();
 	double k = 255;
 	cout << "Enter filename" << endl ;
 	string inputf;
@@ -34,62 +33,50 @@ int main()
 
 	Mat hist_equalized_image;
 	equalizeHist(gray, hist_equalized_image);			//3 улучшаем контраст
-//	imshow("Contrast", hist_equalized_image);
+	imshow("Contrast", hist_equalized_image);
 
 	Mat outputCANNY;
 	Canny(gray,outputCANNY, 10, 100, 3);               //4 метод Канни
-//	imshow("Canny", outputCANNY);
+	imshow("Canny", outputCANNY);
 
 	Mat corners = outputCANNY;
 	vector<Point2f> points;
 	goodFeaturesToTrack(outputCANNY, points, 100, 0.05, 3);               //5 угловые точки и кружочки
 	for (int i = 0; i < points.size(); ++i)
 		circle(corners, points[i], 2, 255, 2, 8, 0);
-	//imshow("Corners", corners);
+	imshow("Corners", corners);
 
 
 	Mat dist;
-	distanceTransform(255-corners, dist, CV_DIST_L2, 5);
-	normalize(dist, dist, 0, 255, NORM_MINMAX);
-//	imshow("Distance", dist);												  //6 карта расстояний
+	distanceTransform(255-outputCANNY, dist, CV_DIST_L2, 5);
+	normalize(dist, dist, 0, 1, NORM_MINMAX);
+	imshow("Distance", dist);	//6 карта расстояний
 	dist.convertTo(dist, CV_8U);
-	normalize(dist, dist, 0, 1, NORM_MINMAX);// 1., NORM_MINMAX);
 
 	Mat integ;
-	integral(input, integ, -1);
-	int height = input.size().height;
-	int width = input.size().width;
-	int channels = input.channels();
+	integral(gray, integ, -1);
 
-	Mat filtered = input.clone();
-	for (int ch = 0; ch < channels; ch++)
-	{
-		for (int i = 0; i < height; i++)
+	Mat filtered = gray;
+	for (int i = 0; i < input.rows; i++)
 		{
-			for (int j = 0; j < width; j++)
+			for (int j = 0; j < input.cols; j++)
 			{
-				int kernelDiam = dist.at<uchar>(i, j);
-				if (kernelDiam % 2 == 0)
-					kernelDiam++;
-				if ((i <= kernelDiam / 2) || (j <= kernelDiam / 2) || (j > width - kernelDiam / 2 - 1) || (i > height - kernelDiam / 2 - 1) || (kernelDiam <= 1))
-					continue;
-				float sum = (integ.at<Vec3f>(i - kernelDiam / 2 - 1, j - kernelDiam / 2 - 1)(ch)
-					- integ.at<Vec3f>(i - kernelDiam / 2 - 1, j + kernelDiam / 2)(ch)
-					+ integ.at<Vec3f>(i + kernelDiam / 2, j + kernelDiam / 2)(ch)
-					- integ.at<Vec3f>(i + kernelDiam / 2, j - kernelDiam / 2 - 1)(ch));
-				filtered.at<Vec3b>(i, j)(ch) = sum / (float)(pow(kernelDiam, 2));
+				int kernel = dist.at<uchar>(i, j);
+				if (kernel % 2 == 0)
+					kernel++;
+				double kernel2 = kernel / 2;
+				if ((kernel <= 1) || (i <= kernel2) || (j <= kernel2) || (i > input.rows - kernel2- 1) || (j > input.cols - kernel2 - 1)) continue;
+				float sum = (integ.at<uchar>(i - kernel2 - 1, j - kernel2 - 1) - integ.at<uchar>(i - kernel2 - 1, j + kernel2)
+						   + integ.at<uchar>(i + kernel2, j + kernel2) - integ.at<uchar>(i + kernel2, j - kernel2 - 1));    //7 фильтрация
+				filtered.at<uchar>(i, j) = sum / (float)(pow(kernel, 2));
 			}
-		}
 	}
 	imshow("Filtered", filtered);
 
-
-	auto end = chrono::high_resolution_clock::now();
-	auto elapsed = end - begin;
-	auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count();
-	cout << endl << "Time spent: " << milliseconds << endl;
-	float Fps = 6 / (milliseconds / 1000.f);
-	cout << "Fps: " << Fps << endl;
+	auto end = high_resolution_clock::now();
+	auto conv = duration_cast<milliseconds>(end - start).count();
+	cout << endl << "Time spent: " << conv << endl;
+	cout << "Fps: " << 6 / (conv / 1000.f) << endl;						//9 Fps
 
 	cvWaitKey(0);
 	return 0;
